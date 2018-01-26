ddb8db789b732ea3d18dc069f9ffc65ec0396fe2
Rick Yoder <RickyYoder@users.noreply.github.com>
2018-01-25 22:35:51 -0500

    Implementing Auth UI

--- public/main.js
+++ public/main.js
@@ -1,28 +1,53 @@
-window.addEventListener('load',function(){
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-	$('.modal').modal();
-	$('.button-collapse').sideNav();
+var uiConfig = {
+  callbacks: {
+    signInSuccess: function(currentUser, credential, redirectUrl) {
+      // User successfully signed in.
+      // Return type determines whether we continue the redirect automatically
+      // or whether we leave that to developer to handle.
+      return false;
+    },
+    uiShown: function() {
+      // The widget is rendered.
+      // Hide the loader.
+      //document.getElementById('loader').style.display = 'none';
+    }
+  },
+  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+  signInFlow: 'popup',
+  signInSuccessUrl: '',
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ],
+  // Terms of service url.
+  tosUrl:''
+};
 
-	$('.fileExplorerTrigger').click(function(){
-	  $('#fileExplorer').modal('open');
-	});
+ui.start('#firebaseui-auth-container', uiConfig);
 
-	$(".downloadTrigger").click(function(){
-	  var blob = new Blob([$("textarea")[0].value],{"type":"text/plain"});
-	  
-	  var url = URL.createObjectURL(blob);
-	  
-	  $("#downloadLink").attr("href",url).attr("download","myFile.txt").click();
-	  
-	  document.getElementById('downloadLink').click();
-	});
+$('.modal').modal();
+$('.button-collapse').sideNav();
 
-	$(".saveTrigger").click(function(){
-	  alert("Not included in alpha version!");
-	});
+$('.fileExplorerTrigger').click(function(){
+  $('#fileExplorer').modal('open');
+});
 
-	$('.close').click(function(){
-	  $('.modal.open').modal('close');
-	});
+$(".downloadTrigger").click(function(){
+  var blob = new Blob([$("textarea")[0].value],{"type":"text/plain"});
+  
+  var url = URL.createObjectURL(blob);
+  
+  $("#downloadLink").attr("href",url).attr("download","myFile.txt").click();
+  
+  document.getElementById('downloadLink').click();
+});
 
+$(".saveTrigger").click(function(){
+  alert("Not included in alpha version!");
+});
+
+$('.close').click(function(){
+  $('.modal.open').modal('close');
 });
